fb8adce746e0bcfc5498e49d54605cddd724ffc8
Benoît Coste <benoit.coste@epfl.ch>
2019-06-21 13:26:21 +0200

    Do not hardcode version
    that's embarrassing
    
    Change-Id: I669610120d5ffcffc3ba57ef5331991b003fb083

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 """Repair setup.py"""
 import os
+import imp
 from setuptools import setup
 from setuptools import find_packages
 
 
-VERSION = "0.1.0"
+VERSION = imp.load_source("repair.version", "repair/version.py").VERSION
 
 REQS = ['click>=0.7.0',
         'scipy>=1.1.0',
