bd78d94c86c4d4ad202376e1b6c7f87657390203
Benoît Coste <benoit.coste@epfl.ch>
2020-09-30 14:03:41 +0200

    Implement Aleksei's comment
    Change-Id: I09bbfc08457845b711aff9de96e3d9eda6319f03

--- tests/test_legacy_detection.py
+++ tests/test_legacy_detection.py
@@ -6,7 +6,7 @@ from numpy.testing import (assert_array_almost_equal, assert_array_equal,
                            assert_equal)
 from morph_tool.spatial import point_to_section_segment
 
-import neuror.cut_plane.legacy_detection as tested
+import neuror.cut_plane.legacy_detection as test_module
 
 DATA_PATH = Path(__file__).parent / 'data'
 
@@ -14,13 +14,13 @@ SIMPLE = load_neuron(DATA_PATH / 'simple.swc')
 
 
 def test_legacy():
-    points, sign = tested.cut_detect(SIMPLE, 'y')
+    points, sign = test_module.cut_detect(SIMPLE, 'y')
     assert_array_equal(points,
                        [[ 6., -4., 0.],
                         [-5., -4., 0.]])
     assert_equal(sign, -1)
 
-    assert_raises(Exception, tested.cut_detect, SIMPLE, 'z')
+    assert_raises(Exception, test_module.cut_detect, SIMPLE, 'z')
 
 def test_legacy_compare_with_legacy_result():
     '''Comparing results with the old repair launch with the following commands:
@@ -30,7 +30,7 @@ def test_legacy_compare_with_legacy_result():
     The arguments are the one used in the legacy morphology workflow.
     '''
     neuron = load_neuron(DATA_PATH / 'compare-bbpsdk/rp120430_P-2_idA.h5')
-    points, sign = tested.cut_detect(neuron, 'z')
+    points, sign = test_module.cut_detect(neuron, 'z')
     assert_equal(sign, 1)
     cut_sections = [point_to_section_segment(neuron, point)[0]
                     for point in points]
