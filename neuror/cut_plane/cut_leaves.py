4fcb99d1ff30b0f912e3a8c89a2d4c3ed3e6c125
arnaudon <alexis.arnaudon@epfl.ch>
2021-03-18 09:44:32 +0100

    more doc/cleanup

--- neuror/cut_plane/cut_leaves.py
+++ neuror/cut_plane/cut_leaves.py
@@ -5,16 +5,31 @@ from neurom.core.dataformat import COLS
 from neuror.cut_plane.planes import HalfSpace
 
 
-def _get_cut_leaves(plane, morphology, bin_width, percentile_threshold):
-    """Compute the cut leaves from a plane."""
+def _get_cut_leaves(half_space, morphology, bin_width, percentile_threshold):
+    """Compute the cut leaves from a given HalfSpace object.
+
+    For the half plane, we find all the cut leaves in the slice of size bin_width,
+    and compute the quality of the cut (see docstring of find_cut_leaves for details).
+    If the quality is positive, a cut is considered valid, and the cut leaves are returned.
+
+    Args:
+        half_space (planes.HalfSpace): half space to search cut points
+        morphology (morphio.Morphology): morphology
+        bin_width: the bin width
+        percentile_threshold: the minimum percentile of leaves counts in bins
+
+    Returns:
+        leaves: ndarray of dim (n, 3) with cut leaves coordinates
+        quality: quality for these cut leaves
+    """
     # get the cut leaves
     leaves = np.array([section for section in morphology.iter() if not section.children])
     leaves_coord = np.array([leaf.points[-1, COLS.XYZ] for leaf in leaves])
-    cut_filter = plane.distance(leaves_coord) < bin_width
+    cut_filter = half_space.distance(leaves_coord) < bin_width
     cut_leaves = leaves[cut_filter]
 
     # compute the min cut leave given the percentile
-    projected_uncut_leaves = plane.project_on_directed_normal(leaves_coord[~cut_filter])
+    projected_uncut_leaves = half_space.project_on_directed_normal(leaves_coord[~cut_filter])
     _min, _max = min(projected_uncut_leaves), max(projected_uncut_leaves)
     bins = np.arange(_min, _max, bin_width)
     _dig = np.digitize(projected_uncut_leaves, bins)
@@ -51,32 +66,38 @@ def find_cut_leaves(
         morph (morphio.Morphology): morphology
         bin_width: the bin width
         percentile_threshold: the minimum percentile of leaves counts in bins
-        searched_axes: x, y or z. Specify the planes for which to search the cut plane
+        searched_axes: x, y or z. Specify the half space for which to search the cut leaves
         searched_half_spaces: A negative value means the morphology lives
                 on the negative side of the plane, and a positive one the opposite.
     Returns:
         ndarray: cut leaves
         list: list of qualities in dicts with axis and side for each
     """
-    # create planes
+    # create half spaces
     searched_axes = [axis.upper() for axis in searched_axes]
-    planes = [
+    half_spaces = [
         HalfSpace(int(axis == "X"), int(axis == "Y"), int(axis == "Z"), 0, upward=(side > 0))
         for axis, side in product(searched_axes, searched_half_spaces)
     ]
 
-    # set the plane coef_d as furthest morphology point
-    for plane, (axis, side) in zip(planes, product(searched_axes, searched_half_spaces)):
-        plane.coefs[3] = -side * np.min(plane.project_on_directed_normal(morph.points), axis=0)
+    # set the half space coef_d as furthest morphology point
+    for half_space, (axis, side) in zip(half_spaces, product(searched_axes, searched_half_spaces)):
+        half_spaces.coefs[3] = -side * np.min(
+            half_spaces.project_on_directed_normal(morph.points), axis=0
+        )
 
-    # find the leaves
-    cuts = [_get_cut_leaves(plane, morph, bin_width, percentile_threshold) for plane in planes]
+    # find the cut leaves
+    cuts = [
+        _get_cut_leaves(half_space, morph, bin_width, percentile_threshold)
+        for half_space in half_spaces
+    ]
 
-    # return only leaves of planes with valid cut
-    leaves = [leave for leave, _ in cuts if leave is not None]
+    # return only cut leaves of half spaces with valid cut
+    _leaves = [leave for leave, _ in cuts if leave is not None]
+    leaves = np.vstack(_leaves) if _leaves else np.array([])
     qualities = [
         {"axis": axis, "side": side, "quality": np.around(quality, 3)}
         for (_, quality), (axis, side) in zip(cuts, product(searched_axes, searched_half_spaces))
         if quality is not None
     ]
-    return np.vstack(leaves) if leaves else [], qualities
+    return leaves, qualities
