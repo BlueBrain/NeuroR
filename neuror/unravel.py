e40944596f8b585989a9c5197ea6c74f22fda3b2
arnaudon <alexis.arnaudon@epfl.ch>
2022-09-13 17:04:15 +0200

    revert black

--- neuror/unravel.py
+++ neuror/unravel.py
@@ -1,7 +1,7 @@
-"""The unravel module
+'''The unravel module
 
 Unravelling is the action of "stretching" the cell that
-has been shrunk because of the dehydratation caused by the slicing"""
+has been shrunk because of the dehydratation caused by the slicing'''
 import json
 import logging
 import os
@@ -17,15 +17,15 @@ from scipy.spatial.ckdtree import cKDTree
 from neuror.cut_plane import CutPlane
 from neuror.utils import RepairJSON
 
-L = logging.getLogger("neuror")
+L = logging.getLogger('neuror')
 
 DEFAULT_WINDOW_HALF_LENGTH = 5
 DEFAULT_WINDOW_HALF_LENGTH_PATH = 8
 
 
 def _get_principal_direction(points):
-    """Return the principal direction of a point cloud
-    It is the eigen vector of the covariance matrix with the highest eigen value"""
+    '''Return the principal direction of a point cloud
+    It is the eigen vector of the covariance matrix with the highest eigen value'''
 
     X = np.copy(np.asarray(points))
     X -= np.mean(X, axis=0)
@@ -35,7 +35,7 @@ def _get_principal_direction(points):
 
 
 def _unravel_section(section, window_half_length, soma, legacy_behavior):
-    """Unravel a section using number of adjacent points as window_half_length"""
+    '''Unravel a section using number of adjacent points as window_half_length'''
     # pylint: disable=too-many-locals
     points = section.points
     if legacy_behavior and section.is_root and len(soma.points) > 1:
@@ -62,7 +62,7 @@ def _unravel_section(section, window_half_length, soma, legacy_behavior):
 
         scalar_product = np.dot(window_direction, direction)
         # point it in the same direction as the window
-        direction *= np.sign(scalar_product or 1.0)
+        direction *= np.sign(scalar_product or 1.)
 
         point = direction + unravelled_points[-1]
         unravelled_points.append(point)
@@ -74,7 +74,7 @@ def _unravel_section(section, window_half_length, soma, legacy_behavior):
 
 # pylint: disable=too-many-locals
 def _unravel_section_path_length(sec, window_half_length, soma, legacy_behavior):
-    """Unravel a section using path length as window_half_length"""
+    '''Unravel a section using path length as window_half_length'''
     unraveled_points = sec.points
     n_points = len(sec.points)
 
@@ -89,7 +89,7 @@ def _unravel_section_path_length(sec, window_half_length, soma, legacy_behavior)
         # find how many segement on the left and right to be close to window_half_length
         intervals = interval_lengths(sec.points)
         _l_left = np.cumsum(intervals[window_center::-1])
-        _l_right = np.cumsum(intervals[min(n_points - 2, window_center) :])
+        _l_right = np.cumsum(intervals[min(n_points - 2, window_center):])
         window_start = np.argmin(abs(_l_left - window_half_length))
         window_end = np.argmin(abs(_l_right - window_half_length))
 
@@ -126,7 +126,7 @@ def _unravel_section_path_length(sec, window_half_length, soma, legacy_behavior)
         # point it in the same direction as the window
         window_direction = sec.points[window_end - 1] - sec.points[window_start]
         scalar_product = np.dot(window_direction, direction)
-        direction *= np.sign(scalar_product or 1.0)
+        direction *= np.sign(scalar_product or 1.)
 
         # update the unravel points
         unraveled_points[window_center] = unraveled_points[window_center - 1] + direction
@@ -136,8 +136,9 @@ def _unravel_section_path_length(sec, window_half_length, soma, legacy_behavior)
         sec.diameters = np.hstack((soma.diameters[0], sec.diameters))
 
 
-def unravel(filename, window_half_length=None, legacy_behavior=False, use_path_length=True):
-    """Return an unravelled neuron
+def unravel(filename, window_half_length=None,
+            legacy_behavior=False, use_path_length=True):
+    '''Return an unravelled neuron
 
     Segment are unravelled iteratively
     Each segment direction is replaced by the averaged direction in a sliding window
@@ -160,16 +161,13 @@ def unravel(filename, window_half_length=None, legacy_behavior=False, use_path_l
         a tuple (morphio.mut.Morphology, dict) where first item is the unravelled
             morphology and the second one is the mapping of each point coordinate
             before and after unravelling
-    """
+    '''
     morph = morphio.Morphology(filename, options=morphio.Option.nrn_order)
-    new_morph = morphio.mut.Morphology(
-        morph, options=morphio.Option.nrn_order
-    )  # noqa, pylint: disable=no-member
+    new_morph = morphio.mut.Morphology(morph, options=morphio.Option.nrn_order)  # noqa, pylint: disable=no-member
 
     if window_half_length is None:
-        window_half_length = (
-            DEFAULT_WINDOW_HALF_LENGTH_PATH if use_path_length else DEFAULT_WINDOW_HALF_LENGTH
-        )
+        window_half_length = DEFAULT_WINDOW_HALF_LENGTH_PATH \
+            if use_path_length else DEFAULT_WINDOW_HALF_LENGTH
 
     coord_before = np.empty([0, 3])
     coord_after = np.empty([0, 3])
@@ -185,67 +183,63 @@ def unravel(filename, window_half_length=None, legacy_behavior=False, use_path_l
         coord_after = np.append(coord_after, new_section.points, axis=0)
 
         if legacy_behavior and len(morph.soma.points) > 1:  # pylint: disable=no-member
-            coord_before = np.vstack(
-                (morph.soma.points[0], coord_before)
-            )  # noqa, pylint: disable=no-member
-        mapping = pd.DataFrame(
-            {
-                "x0": coord_before[:, 0],
-                "y0": coord_before[:, 1],
-                "z0": coord_before[:, 2],
-                "x1": coord_after[:, 0],
-                "y1": coord_after[:, 1],
-                "z1": coord_after[:, 2],
-            }
-        )
+            coord_before = np.vstack((morph.soma.points[0], coord_before))  # noqa, pylint: disable=no-member
+        mapping = pd.DataFrame({
+            'x0': coord_before[:, 0],
+            'y0': coord_before[:, 1],
+            'z0': coord_before[:, 2],
+            'x1': coord_after[:, 0],
+            'y1': coord_after[:, 1],
+            'z1': coord_after[:, 2],
+        })
 
-    L.debug("Unravel successful for file: %s", filename)
+    L.debug('Unravel successful for file: %s', filename)
     return new_morph, mapping
 
 
 def unravel_plane(plane, mapping):
-    """Return a new CutPlane object where the cut-leaves
-    position has been updated after unravelling"""
+    '''Return a new CutPlane object where the cut-leaves
+    position has been updated after unravelling'''
     leaves = plane.cut_leaves_coordinates
 
     if not np.any(leaves):
         return plane
 
-    t = cKDTree(mapping[["x0", "y0", "z0"]])
+    t = cKDTree(mapping[['x0', 'y0', 'z0']])
     distances, indices = t.query(leaves)
     not_matching_leaves = np.where(distances > 1e-5)[0]
     if not_matching_leaves.size:
-        raise Exception(
-            "Cannot find the following leaves in the mapping:\n" + str(leaves[not_matching_leaves])
-        )
-    plane.cut_leaves_coordinates = mapping.iloc[indices][["x1", "y1", "z1"]].values
+        raise Exception('Cannot find the following leaves in the mapping:\n' +
+                        str(leaves[not_matching_leaves]))
+    plane.cut_leaves_coordinates = mapping.iloc[indices][['x1', 'y1', 'z1']].values
     return plane
 
 
-def unravel_all(
-    raw_dir, unravelled_dir, raw_planes_dir, unravelled_planes_dir, window_half_length=None
-):
-    """Repair all morphologies in input folder"""
+def unravel_all(raw_dir, unravelled_dir,
+                raw_planes_dir,
+                unravelled_planes_dir,
+                window_half_length=None):
+    '''Repair all morphologies in input folder
+    '''
     if not os.path.exists(raw_planes_dir):
-        raise Exception(f"{raw_planes_dir} does not exist")
+        raise Exception(f'{raw_planes_dir} does not exist')
 
     if not os.path.exists(unravelled_planes_dir):
         os.mkdir(unravelled_planes_dir)
 
     for inputfilename in iter_morphology_files(raw_dir):
-        L.debug("Unravelling: %s", inputfilename)
+        L.debug('Unravelling: %s', inputfilename)
         outfilename = Path(unravelled_dir, inputfilename.name)
         raw_plane = CutPlane.from_json(
-            Path(raw_planes_dir, inputfilename.name).with_suffix(".json")
-        )
-        unravelled_plane = Path(unravelled_planes_dir, inputfilename.name).with_suffix(".json")
+            Path(raw_planes_dir, inputfilename.name).with_suffix('.json'))
+        unravelled_plane = Path(unravelled_planes_dir, inputfilename.name).with_suffix('.json')
 
         try:
             neuron, mapping = unravel(str(inputfilename), window_half_length)
             neuron.write(str(outfilename))
-            with open(str(unravelled_plane), "w") as f:
+            with open(str(unravelled_plane), 'w') as f:
                 json.dump(unravel_plane(raw_plane, mapping).to_json(), f, cls=RepairJSON)
 
         except Exception as e:  # noqa, pylint: disable=broad-except
-            L.warning("Unravelling %s failed", inputfilename)
+            L.warning('Unravelling %s failed', f)
             L.warning(e, exc_info=True)
